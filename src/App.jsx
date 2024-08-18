3062c7abf848c2ff19e5b23b7099dba32ad41843
arthur voryshev <what_to_write@mail.ru>
2024-08-19 00:07:20 +1000

    add spell collision logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,68 @@
 import Canvas from './Canvas';
+// import ChangeColor from './menuChangeColor';
+import './App.css'
+import React, { useRef, useState } from 'react';
+import ReactDOM from 'react-dom'
 
 function App() {
+  const [isPortalVisible, setIsPortalVisible] = useState(false);
+  const [portalPosition, setPortalPosition] = useState({ top: 0, left: 0 });
+
+  const renderPortal = () => {
+    const portalContainer = document.getElementById('portal');
+    if (!(portalContainer && isPortalVisible)) {
+      return null;
+    }
+
+    return ReactDOM.createPortal(
+      <div style={{ position: 'absolute', ...portalPosition, border: '2px solid red' }}>
+        choose color modal (aka menu)
+      </div>,
+      portalContainer,
+    );
+  };
+
+  const pauseGame = () => {
+    console.log('PAUSE!!!')
+    const currentCanvas = document.getElementById('canvas');
+    if (!currentCanvas) {
+      console.log('no canvas');
+    }
+
+    const currentContext = currentCanvas.getContext('2d');
+    currentContext.state = { ...currentContext.state, isPaused: true };
+    // console.log(currentContext.state);
+  };
+
+  const resumeGame = () => {
+    console.log('RESUME!!!')
+    const currentCanvas = document.getElementById('canvas');
+    if (!currentCanvas) {
+      console.log('no canvas');
+    }
+
+    const currentContext = currentCanvas.getContext('2d');
+    currentContext.state = { ...currentContext.state, isPaused: false };
+    // console.log(currentContext.state);
+  };
+
   return (
-    <Canvas width="800" height="500" />
+    <>
+      <div>
+        {/* <ChangeColor /> */}
+        <button onClick={(event) => {
+          // console.log(event.target.getBoundingClientRect())
+          setPortalPosition({ top: event.target.getBoundingClientRect().top + 25, left: event.target.getBoundingClientRect().left + 50 });
+          setIsPortalVisible(previousValue => {
+            return !previousValue;
+          });
+
+          isPortalVisible ? resumeGame() : pauseGame();
+        }}>avatar</button>
+        <Canvas width="800" height="500" />
+      </div>
+      {renderPortal()}
+    </>
   );
 }
 
--- src/Canvas.js
+++ src/Canvas.js
@@ -1,17 +1,21 @@
 import { useEffect, useRef, useState } from "react"
+import ChangeColor from "./menuChangeColor";
+// import changeColor from './menuChangeColor';
+
 
 const Canvas = props => {
+  // pused gamea
   const ref = useRef(null);
 
   const changePlayer1Color = (color) => {
     const currentCanvas = ref.current;
     if (!currentCanvas) {
-      console.log('no canvas');
+      // console.log('no canvas');
     }
 
     const currentContext = currentCanvas.getContext('2d');
     currentContext.state = { player1Color: color };
-    console.log(currentContext.state);
+    // console.log(currentContext.state);
   };
 
   useEffect(() => {
@@ -28,20 +32,20 @@ const Canvas = props => {
         ballRadius: 20,
       },
       player2: {
-        x: canvas.width-24,
+        x: canvas.width - 24,
         y: 26,
         dx: 0,
         dy: 5,
         ballRadius: 20,
       }
     }
-    
+
     // spells
     let magicBallRadius = 10
     let spellsP1 = []; // [{ x: number, y: number }, ...]
     let spellsP2 = []
-    let spellCastingSpeedP1 = 1500;
-    let spellCastingSpeedP2 = 1500;
+    let spellCastingSpeedP1 = 500;
+    let spellCastingSpeedP2 = 500;
     let lastSpellCastedAtP1 = Date.now();
     let lastSpellCastedAtP2 = Date.now();
 
@@ -50,55 +54,62 @@ const Canvas = props => {
     let mouseY = 0;
 
     // field
-    const field = () =>  {
+    const field = () => {
       context.fillStyle = '#e4cce6'
-      context.fillRect(0,0,1000,1000)
+      context.fillRect(0, 0, 1000, 1000)
     }
 
     // duelistFirst
     const duelistFirst = () => {
       context.beginPath();
-      context.arc(players.player1.x, players.player1.y, players.player1.ballRadius, 0, Math.PI*2);
+      context.arc(players.player1.x, players.player1.y, players.player1.ballRadius, 0, Math.PI * 2);
       context.fillStyle = context.state && context.state.player1Color ? context.state.player1Color : "#0095DD";
       context.fill();
       context.closePath();
     }
+  
     // duelistSecond
     const duelistSecond = () => {
       context.beginPath();
-      context.arc(players.player2.x, players.player2.y, players.player2.ballRadius, 0, Math.PI*2);
+      context.arc(players.player2.x, players.player2.y, players.player2.ballRadius, 0, Math.PI * 2);
       context.fillStyle = '#f5c00f' // "#0095DD";
       context.fill();
       context.closePath();
     }
-    
+
     // magic
     const magic = ({ x, y }) => {
       context.beginPath();
-      context.arc(x, y, magicBallRadius, 0, Math.PI*2);
+      context.arc(x, y, magicBallRadius, 0, Math.PI * 2);
       context.fillStyle = "#db7b82";
       context.fill();
       context.closePath();
     }
+
+    let countP1 = 0
+    let countP2 = 0
 
     const getCanCastSpellP1 = () => Date.now() - spellCastingSpeedP1 >= lastSpellCastedAtP1;
     const getCanCastSpellP2 = () => Date.now() - spellCastingSpeedP2 >= lastSpellCastedAtP2;
 
     // start game
     const draw = () => {
+      // console.log('isPaused', context.state?.isPaused);
+
       context.clearRect(0, 0, canvas.width, canvas.height)
 
       // edge collision player1
       players.player1.y += players.player1.dy;
       if (getCanCastSpellP1()) {
+        // console.log('getCanCastSpellP1');
         lastSpellCastedAtP1 = Date.now();
         spellsP1.push({ x: players.player1.x, y: players.player1.y });
       }
 
       if (players.player1.y + 50 >= 535 || players.player1.y <= 30) {
-        players.player1.dy = -players.player1.dy; 
+        players.player1.dy = -players.player1.dy;
       }
-       // edge collision player2
+      // edge collision player2
       players.player2.y += players.player2.dy;
       if (getCanCastSpellP2()) {
         lastSpellCastedAtP2 = Date.now();
@@ -106,7 +117,7 @@ const Canvas = props => {
       }
 
       if (players.player2.y + 50 >= 535 || players.player2.y <= 30) {
-        players.player2.dy = -players.player2.dy; 
+        players.player2.dy = -players.player2.dy;
       }
       // mouse collision player1
       if (
@@ -127,7 +138,25 @@ const Canvas = props => {
         players.player2.dy = -players.player2.dy;
       }
 
-      // spell collision P1
+      // spell collision 
+      const isSpellCollision = (spellX, spellY, spellRadius, playerX, playerY, playerWidth, playerHeight ) => {
+        if (
+          spellX >= playerX &&
+          spellX <= playerX + playerWidth &&
+          spellY >= playerY &&
+          spellY <= playerY + playerHeight
+      ) {
+          return true;
+      }
+      const closestX = Math.max(playerX, Math.min(spellX, playerX + playerWidth));
+        const closestY = Math.max(playerY, Math.min(spellY, playerY + playerHeight));
+        const distance = Math.sqrt(
+            Math.pow(closestX - spellX, 2) + Math.pow(closestY - spellY, 2)
+        );
+        return distance <= spellRadius;
+    };
+
+  
 
       // change color
       const handleClick = (event) => {
@@ -136,52 +165,76 @@ const Canvas = props => {
         // if (mouseX === players.player1.x || mouseY === players.player2.y ) {
         // }
 
-      //   const colorX = event.clientX - rect.left;
-      //   const colorY = event.clientY - rect.top;
-      //   // Проверяем, находится ли клик внутри прямоугольника
-      // if (colorX >= 50 && colorX <= 150 && colorY >= 50 && colorY <= 150) {
-      //   // Изменяем цвет фигуры на случайный цвет
-      // }
-    };
+        //   const colorX = event.clientX - rect.left;
+        //   const colorY = event.clientY - rect.top;
+        //   // Проверяем, находится ли клик внутри прямоугольника
+        // if (colorX >= 50 && colorX <= 150 && colorY >= 50 && colorY <= 150) {
+        //   // Изменяем цвет фигуры на случайный цвет
+        // }
+      };
 
-
-      
       field()
       duelistFirst()
       duelistSecond()
       spellsP1 = spellsP1.map(spell => ({ ...spell, x: spell.x + 5 }));
-      spellsP1.forEach(magic); // TODO: combine with spells.map(...)
+      spellsP1.forEach((spell) => {
+        if (isSpellCollision(spell.x, spell.y, magicBallRadius, players.player2.x, players.player2.y, 0, Math.PI * 2)) {
+          spell.x = 0
+          spell.y = 0
+          countP1 += 1
+          
+          console.log('p1', countP1)
+        } else {
+          magic(spell)
+        }
+      }); // TODO: combine with spells.map(...)
       spellsP2 = spellsP2.map(spell => ({ ...spell, x: spell.x - 5 }));
-      spellsP2.forEach(magic); // TODO: combine with spells.map(...)
-        
-      requestAnimationFrame(draw);
-      // canvas.addEventListener('click', handleClick);
+      spellsP2.forEach((spell) => {
+        if (isSpellCollision(spell.x, spell.y, magicBallRadius, players.player1.x, players.player1.y, 0, Math.PI * 2)) {
+          spell.x = 0
+          spell.y = 0
+          countP2 += 1
 
-    };
-    
-      canvas.addEventListener('mousemove', (event) => {
-        mouseX = event.offsetX;
-        mouseY = event.offsetY;
-        // console.log('mouseX', event.offsetX)
-        // console.log('mouseY', event.offsetY)
+          console.log('p2', countP2)
+        } else {
+          magic(spell)
+        }
       });
+      // winner
+      if (countP1 > 2) {
+        console.log(`Winner Player 1`)
+      } else if (countP2 > 2) {
+        console.log(`Winner Player 2`)
+      } else {
+        requestAnimationFrame(draw);
+      }
+      // if (!context.state?.isPaused) {
+      // }
+      // canvas.addEventListener('click', handleClick);
+    
+    }
+    canvas.addEventListener('mousemove', (event) => {
+      mouseX = event.offsetX;
+      mouseY = event.offsetY;
+      // console.log('mouseX', event.offsetX)
+      // console.log('mouseY', event.offsetY)
+    });
 
 
     draw()
   }, []);
 
+
   return (
     <div>
       <div>
-        <input type="color" id="head" name="head" value="#0095DD" />
-        <label for="head">  count</label>
-        <button onClick={() => changePlayer1Color('#FFFFFF')}> cccc</button>
+        <button className="p1" onClick={() => changePlayer1Color('#FFFFFF')}>player 1</button>
       </div>
       <div>
-        <input type="color" id="body" name="body" value="#f6b73c" />
-        <label for="body">  count</label>
+        <button className="p2" onClick={() => <ChangeColor />}>player 2</button>
       </div>
-      <canvas ref={ref} {...props} />
+        {/* <changeColor /> */}
+      <canvas id='canvas' ref={ref} {...props} />
     </div>
   )
 }
